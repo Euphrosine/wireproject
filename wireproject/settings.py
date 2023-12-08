f382530a05d5b79695bd5158d273d42298d6e5b0
Euphrosine <euphrosine46@.com>
2023-12-08 07:08:16 -0800

    chages

--- wireproject/settings.py
+++ wireproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t)gf9ypfj2h3(#cts7xawsc)cvz&7s%@&mt^+#vf+n%i^-qn*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
